77794514fb70a13984a3aaf2e6b58933b44024e9
lopagabri-gif <lopagabri@gmail.com>
2026-05-21 21:15:43 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "meteo-pisticci-v4";
+const CACHE_NAME = "meteo-pisticci-v5";
 
 const urlsToCache = [
   "/",
@@ -34,4 +34,16 @@ return response || fetch(event.request);
 })
 );
 
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
 });
