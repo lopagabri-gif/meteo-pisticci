4445c676cfc7d0a7ca1baa2a6adcc535417a321e
lopagabri-gif <lopagabri@gmail.com>
2026-05-22 02:33:03 +0200

    Update marconia.js

--- api/marconia.js
+++ api/marconia.js
@@ -1,26 +1,27 @@
 export default async function handler(req, res) {
-  res.setHeader("Cache-Control", "no-store");
-  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  const apiKey = "badc97819d3b4f3e9c97819d3b6f3eee";
+  const stationId = "IBASILIC18";
 
   try {
-    const apiKey = "badc97819d3b4f3e9c97819d3b6f3eee";
-    const stationId = "IBASILIC18";
 
-    const url =
-      "https://api.weather.com/v2/pws/observations/current?stationId=" +
-      stationId +
-      "&format=json&units=m&apiKey=" +
-      apiKey;
+    const response = await fetch(
+      `https://api.weather.com/v2/pws/observations/current?stationId=${stationId}&format=json&units=m&apiKey=${apiKey}`
+    );
 
-    const response = await fetch(url);
     const data = await response.json();
 
-    return res.status(200).json(data);
+    res.setHeader("Access-Control-Allow-Origin", "*");
 
-  } catch (error) {
-    return res.status(500).json({
+    res.status(200).json(data);
+
+  } catch (err) {
+
+    res.status(500).json({
       error: "Errore dati Marconia",
-      details: error.message
+      details: err.message
     });
+
   }
+
 }
