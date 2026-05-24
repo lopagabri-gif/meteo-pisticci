80d4dbc285d4418add6622a905797ed7b2bafb7c
lopagabri-gif <lopagabri@gmail.com>
2026-05-25 01:34:04 +0200

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,15 @@
+importScripts('https://www.gstatic.com/firebasejs/12.13.0/firebase-app-compat.js');
+
+importScripts('https://www.gstatic.com/firebasejs/12.13.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+apiKey: "AIzaSyB3KfNcPZb42EFNANr1vu5qLaBQJBSRXnU",
+authDomain: "meteo-pisticci.firebaseapp.com",
+projectId: "meteo-pisticci",
+storageBucket: "meteo-pisticci.firebasestorage.app",
+messagingSenderId: "383061112581",
+appId: "1:383061112581:web:c25eb6292342f96bcee96c",
+measurementId: "G-NPS7Q9EBKG"
+});
+
+const messaging = firebase.messaging();
