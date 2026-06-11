76385c001fc6d6c11e89a5ed01bf469cc061d7f5
lopagabri-gif <lopagabri@gmail.com>
2026-06-11 13:16:16 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -22,7 +22,7 @@ messaging.onBackgroundMessage(function(payload) {
   );
 });
 
-const CACHE_NAME = "meteo-pisticci-v35";
+const CACHE_NAME = "meteo-pisticci-v36";
 const urlsToCache = ["/manifest.json"];
 
 self.addEventListener("install", event => {
